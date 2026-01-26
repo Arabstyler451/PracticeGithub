77054d013bd21a17c600cf558b51cfc83d4917d3
Arabstyler451 <0612abna@gmail.com>
2026-01-26 20:27:48 +0000

    Home
    Nothiing

--- GithubProject/GithubProject/obj/Debug/net9.0/GithubProject.AssemblyInfo.cs
+++ GithubProject/GithubProject/obj/Debug/net9.0/GithubProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithubProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae0bad82fec47ff3b90244dc964f7b1dc3ad6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c54c6eb9687a482e6ff59400e7e7d32c82cfb15")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithubProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithubProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
